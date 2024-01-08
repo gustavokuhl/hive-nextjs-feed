fde81e2e9b01236b5116a9559be9faca2445c969
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-01-07 21:33:56 -0300

    feat: theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/ui/mode-toogle"
 import { useState } from "react"
 
 export default function Home() {
   const [count, setCount] = useState(0)
   return (
-    <main className="flex w-full h-screen justify-center items-center">
+    <main className="flex w-full h-screen justify-center items-center gap-2">
       <Button
         onClick={() => {
           setCount((count) => count + 1)
@@ -14,6 +15,7 @@ export default function Home() {
       >
         Clicked {count} time{count == 1 || "s"}
       </Button>
+      <ModeToggle />
     </main>
   )
 }
