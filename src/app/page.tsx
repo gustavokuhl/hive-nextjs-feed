8d41cbcaeb1a23c2c0056ad14c931b842e53b7c6
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-01-07 22:06:50 -0300

    feat: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import { ModeToggle } from "@/components/ui/mode-toogle"
+import Navbar from "@/components/navbar"
 import { useState } from "react"
 
 export default function Home() {
   const [count, setCount] = useState(0)
   return (
-    <main className="flex w-full h-screen justify-center items-center gap-2">
-      <Button
-        onClick={() => {
-          setCount((count) => count + 1)
-        }}
-      >
-        Clicked {count} time{count == 1 || "s"}
-      </Button>
-      <ModeToggle />
+    <main className="max-w-[700px] mx-auto min-h-screen">
+      <Navbar />
     </main>
   )
 }
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,18 @@
+import { usePathname } from "next/navigation"
+import { ModeToggle } from "../ui/mode-toogle"
+
+function getTitleForPath(path: string) {
+  if (path === "/") return "Feed"
+}
+
+export default function Navbar() {
+  const pathname = usePathname()
+  return (
+    <nav className="mt-2 px-2 md:mt-4 scroll-m-20 border-b pb-2 flex justify-between items-center">
+      <h2 className="text-3xl font-semibold tracking-tight transition-colors">
+        {getTitleForPath(pathname)}
+      </h2>
+      <ModeToggle />
+    </nav>
+  )
+}
